e05f5941399f0a0d6655d352458d43872310f809
rian-07 <yunalily07@gmail.com>
2025-07-13 13:33:42 +0900

    Update home.js

--- js/home.js
+++ js/home.js
@@ -75,7 +75,7 @@ onAuthStateChanged(auth, async (user) => {
         if (todoSnap.empty) {
             const emptyMsg = document.createElement('li');
             emptyMsg.textContent = '오늘 할 일이 없습니다.';
-            emptyMsg.style.color = '#888';
+            emptyMsg.style.color = 'white';
             todoListEl.appendChild(emptyMsg);
         } else {
             todoSnap.forEach(doc => {
